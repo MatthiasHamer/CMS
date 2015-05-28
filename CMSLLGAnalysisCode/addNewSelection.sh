a1eef79a58568347fca38b7b2b37803aac953b1f
MatthiasHamer <matthias.hamer@cern.ch>
2015-05-28 21:08:05 +0200

    added script to add new cutflows

---
+++ CMSLLGAnalysisCode/addNewSelection.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "#include \"LLGAnalysis.h\"" >> cutflow${1}.cpp
+echo "void LLGAnalysis::Setup${1}() {" >> cutflow${1}.cpp
+echo  >> cutflow${1}.cpp
+echo "    // setup the cutflow">> cutflow${1}.cpp
+echo  >> cutflow${1}.cpp
+echo "    // and the histograms" >> cutflow${1}.cpp
+echo  >> cutflow${1}.cpp
+echo "    return;">> cutflow${1}.cpp
+echo "}">> cutflow${1}.cpp
+echo  >> cutflow${1}.cpp
+echo "void LLGAnalysis::${1}Selection() {">> cutflow${1}.cpp
+echo  >> cutflow${1}.cpp
+echo "    return;">> cutflow${1}.cpp
+echo "}">> cutflow${1}.cpp
+
+
+sed -ie '/INSERT YOUR SELECTION HERE/ i\        void Setup'${1}'();' LLGAnalysis.h
+sed -ie '/INSERT YOUR SELECTION HERE/ i\        void '${1}'Selection();' LLGAnalysis.h
+sed -ie '/SETUP YOUR SELECTION HERE/ i\    else if( SELECTION == "'${1}'" ) Setup'${1}'();' LLGAnalysis.cpp
+sed -ie '/CALL YOUR SELECTION HERE/ i\        else if( SELECTION == "'${1}'" ) '${1}'Selection();' LLGAnalysis.cpp
+sed -ie 's/cutflowSignalRegion.o/cutflowSignalRegion.o cutflowWJetsCR.o/' Makefile
---
+++ CMSLLGAnalysisCode/cutflowWJetsCR.cpp
@@ -0,0 +1,14 @@
+#include "LLGAnalysis.h"
+void LLGAnalysis::SetupWJetsCR() {
+
+    // setup the cutflow
+
+    // and the histograms
+
+    return;
+}
+
+void LLGAnalysis::WJetsCRSelection() {
+
+    return;
+}
